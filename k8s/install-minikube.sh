1dd205fd3af753d70296b8b3b65b2411a6ac758f
remiforest <remi.forest@akilio.com>
2018-10-19 14:16:46 +0200

    updated to have everything on same node

--- k8s/install-minikube.sh
+++ k8s/install-minikube.sh
@@ -1,9 +1,11 @@
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64&& chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube -f
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.10.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo cp kubectl /usr/local/bin/ && rm kubectl -f
+#! /bin/bash
 
-yum install -y yum-utils  device-mapper-persistent-data lvm2
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm -f minikube
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.10.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo cp kubectl /usr/local/bin/ && rm -f kubectl
+yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce -y
-
+sudo yum install -y docker-ce
 systemctl enable docker.service
 systemctl start docker
+
+minikube start --vm-driver=none
